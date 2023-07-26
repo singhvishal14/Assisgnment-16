a05fcb076d0d48e568c38a7a701ac5df02d806aa
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-26 15:36:53 +0530

    initial Commit

---
+++ BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment_16
+{
+    public class BankAccount
+    {
+      public BankAccount(string Name,int num)
+        {
+            this.AccountNumber = num;
+            this.AccountHolderName = Name;
+            this.Balance = 0;
+        }
+        private decimal Balance;
+        public decimal balance
+        {
+            get { return Balance; } 
+        }
+        private readonly int AccountNumber;
+        public int accountNumber
+        {
+            get { return AccountNumber; } }
+
+        private string AccountHolderName;
+        public string accountHolderName
+        {
+            get { return AccountHolderName; }
+            set { AccountHolderName = value; }
+        }
+        
+        public void Deposit(decimal amount)
+        {
+            if (amount >0)
+            {
+                Balance += amount;
+                Console.WriteLine($"Deposit {amount} \n New Balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine($"Please Put Amount Which is greater than Zero");
+            }
+           
+
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount > 0 && amount <=balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Withdraw Money {amount} \n New Balance: {balance}");
+            }
+            else {
+                Console.WriteLine($"Please Put Amount Which is greater than Zero");
+            }
+            
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("Vishal",123);
+            Console.WriteLine($"AccountHolder Name  is: {account.accountHolderName} Account Number is: {account.accountNumber}");
+            
+            Console.WriteLine($"Balance is: {account.balance}");
+            Console.WriteLine("Please Enter Deposit Money");
+            account.Deposit(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine($"Money After Deposit: {account.balance}");
+            Console.WriteLine("Please Show Withdrawn Money");
+            account.Withdraw(decimal.Parse(Console.ReadLine()));
+            Console.WriteLine($"Money After Withdraw: {account.balance}");
+            Console.ReadKey();
+        }
+    }
+}
